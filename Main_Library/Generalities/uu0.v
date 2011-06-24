3a31b31e4dc9bc23284540f44598f1ced899f45c
Daniel R. Grayson <drg@illinois.edu>
2011-06-24 14:33:55 +0200

    improve the proof of isweql1

--- Main_Library/Generalities/uu0.v
+++ Main_Library/Generalities/uu0.v
@@ -1,43 +1,154 @@
 (** * Introduction 
 
-This is the first of the group of files which contain the (current state of) the mathematical library for the proof assistant Coq based on the Univalent Foundations.  
-Univalent Foundations are inspired by the univalent model of type theory which interprets types as homotopy types, Martin-Lof equality as paths spaces and universes as bases of universal ("univalent") fibrations. For a detailed overview of the content see the table of content in univ_toc.html . In has been modified from the eralier version in a several places but most importantly the part of the earlier file concerning the h-version of the inhabited construction and constructions related to finite sets have been moved to separate files.  
+This is the first of the group of files which contain the (current state of) the mathematical library for the proof assistant Coq based on the Univalent Foundations.
+  
+    
+Univalent Foundations are inspired by the univalent model of type theory which interprets types as homotopy types, Martin-Lof equality as paths spaces and universes as bases of universal ("univalent") fibrations.
+  
+   For a detailed overview of the content see the table of content in univ_toc.
+  
+  html .
+  
+   In has been modified from the eralier version in a several places but most importantly the part of the earlier file concerning the h-version of the inhabited construction and constructions related to finite sets have been moved to separate files.
+  
+    
 
 I tried to keep the notations such that the names of types which are (expected to be) a property in the sense of being of h-level 1 start with "is" but I have not been very consistent about it.
+  
+  
 
-There is an increasing number of theorems which have very short proofs based on the univalence axiom  which are given much longer proofs here. One hopes that eventually a mechnaical procedure for the replacement of proofs using the univalence axiom by proofs which only use it in some computationally uninformative ways will be found but until then I am not using the univalence axiom in any of the constructions. 
+There is an increasing number of theorems which have very short proofs based on the univalence axiom  which are given much longer proofs here.
+  
+   One hopes that eventually a mechnaical procedure for the replacement of proofs using the univalence axiom by proofs which only use it in some computationally uninformative ways will be found but until then I am not using the univalence axiom in any of the constructions.
+  
+   
 
 
-IMPORTANT: for those who may want to add to these files. There are some rules which have to be followed in creating new definitions and theorems which at the moment are not tracked by the proof checker.
+IMPORTANT: for those who may want to add to these files.
+  
+   There are some rules which have to be followed in creating new definitions and theorems which at the moment are not tracked by the proof checker.
+  
+  
 
-1. The current system of Coq is not completely compatible with the univalent semantics. The problem (the only one as far as I know) lies in the way the universe levels (u-levels) are assigned to the objects defined by the inductive definitions of the form
+1.
+  
+   The current system of Coq is not completely compatible with the univalent semantics.
+  
+   The problem (the only one as far as I know) lies in the way the universe levels (u-levels) are assigned to the objects defined by the inductive definitions of the form
 
-Inductive Ind (...)...(...): A -> Type := ...
+Inductive Ind (.
+  
+  .
+  
+  .
+  
+  ).
+  
+  .
+  
+  .
+  
+  (.
+  
+  .
+  
+  .
+  
+  ): A -> Type := .
+  
+  .
+  
+  .
+  
+  
 
 
-The current u-level assignemet takes into account the u-levels of the constructors but not the u-level of A. To ensure compatibility with the univalent model the u-level of Ind should be no less than the u-level of A. The u-levels of the parameters (the types appearing in (...)...(...) ) do not matter. 
+The current u-level assignemet takes into account the u-levels of the constructors but not the u-level of A.
+  
+   To ensure compatibility with the univalent model the u-level of Ind should be no less than the u-level of A.
+  
+   The u-levels of the parameters (the types appearing in (.
+  
+  .
+  
+  .
+  
+  ).
+  
+  .
+  
+  .
+  
+  (.
+  
+  .
+  
+  .
+  
+  ) ) do not matter.
+  
+   
 
-A particular case of this problem is the "singleton elimination" rule which provides a good example of this incompatibility. The inductive definition of the identity types which uses one of the points as a parametr has A=:T (the underlying type) but since no mention of T appears in the constructor the system considers that there are no u-level restrictions on the resulting object and in particular that it can be placed in Prop while still having the full Ind_rect elimninator (see elimination, singleton elimination in the Index to the Refernce Manual). 
+A particular case of this problem is the "singleton elimination" rule which provides a good example of this incompatibility.
+  
+   The inductive definition of the identity types which uses one of the points as a parametr has A=:T (the underlying type) but since no mention of T appears in the constructor the system considers that there are no u-level restrictions on the resulting object and in particular that it can be placed in Prop while still having the full Ind_rect elimninator (see elimination, singleton elimination in the Index to the Refernce Manual).
+  
+   
 
 Since in the present approach the universe management is made explicit one has:
 
 RULE 1 Do not use inductive definitions of the form 
 
-Inductive Ind (...)...(...): A -> UU := ...
+Inductive Ind (.
+  
+  .
+  
+  .
+  
+  ).
+  
+  .
+  
+  .
+  
+  (.
+  
+  .
+  
+  .
+  
+  ): A -> UU := .
+  
+  .
+  
+  .
+  
+  
 
-unless all the "arguments" of A can be typed to UU. 
+unless all the "arguments" of A can be typed to UU.
+  
+   
 
 
-2. While it does not lead directly to any contradictions the shape of the foundations suggests very strongly that at the moment it is best to completely avoid the use of the universes Prop and Set. Instead we should use the the conditions isaprop and isaset which are applicable to the types of any of the type universes.  
+2.
+  
+   While it does not lead directly to any contradictions the shape of the foundations suggests very strongly that at the moment it is best to completely avoid the use of the universes Prop and Set.
+  
+   Instead we should use the the conditions isaprop and isaset which are applicable to the types of any of the type universes.
+  
+    
 
 *)
 
 
 
 
-(** Preambule. *)
+(** Preambule.
+  
+   *)
 
-Unset Automatic Introduction.  (** This line has to be removed for the file to compile with Coq8.2 *)
+Unset Automatic Introduction.
+    (** This line has to be removed for the file to compile with Coq8.2 *)
 
 Definition UU:= Type.
 
@@ -572,10 +683,15 @@ set (int1:=constr1 Y (fun y:Y => paths X (g y) z) y (f z) eint). destruct int1 a
 set (int2:=hfiberpair _ _ (fun x0 : X => g (f x0)) z z (t e)).   split with int2.  apply x.  Defined. 
 
 
-Lemma isweql1  (X:UU)(Y:UU)(f:X -> Y)(g: Y-> X)(efg: forall y:Y, paths Y (f(g y)) y) (z: X): iscontr (hfiber _ _ (fun x:X => g(f x)) z) ->iscontr (hfiber _ _ g z).
-Proof. intros. set (X1:= hfiber _ _ (fun x:X => g(f x)) z). set (Y1:= hfiber _ _ g z). set (f1:= hfibersgftog _ _ _ f g z). set (g1:= fun z0:_ => pr21 _ _ (constr2 _ _ f g efg z z0)). 
-set (efg1:= (fun y1:Y1 => pr22 _ _ (constr2 _ _ f g efg z y1))).  simpl in efg1. apply (contrl1 _ _ f1 g1 efg1). assumption.   Defined. 
-
+Lemma isweql1 (X:UU)(Y:UU)(f:X -> Y)(g: Y-> X) : 
+        (forall y, paths _ (f(g y)) y) -> forall  z: X, iscontr (hfiber _ _ (compose g f) z) -> iscontr (hfiber _ _ g z).
+Proof.
+   intros X Y f g efg z.
+   exact (contrl1 _ _ 
+     (hfibersgftog _ _ _ f g z)
+     (fun z' => pr21 _ _ (constr2 _ _ _ _ efg z z'))
+     (fun y' => pr22 _ _ (constr2 _ _ _ _ efg z y'))).
+   Defined.
 
 Lemma isweql2 (X:UU)(Y:UU)(f1:X-> Y) (f2:X->Y) (h: forall x:X, paths _ (f2 x) (f1 x))(y:Y): iscontr (hfiber _ _ f2 y) -> iscontr (hfiber _ _ f1 y).
 Proof. intros X Y f1 f2 h y X0. 
@@ -602,11 +718,11 @@ Proof. intros X Y f1 f2 h X0. unfold isweq. intro. set (Y0:= X0 y).  apply (iswe
 
 
 Theorem gradth (X:UU)(Y:UU)(f:X->Y)(g:Y->X)(egf: forall x:X, paths _ (g (f x)) x)(efg: forall y:Y, paths _ (f (g y)) y ): isweq _ _ f.
-Proof. intros.  unfold isweq.  intros. rename y into z. 
-assert (iscontr (hfiber _ _ (fun y:Y => (f (g y))) z)). 
-assert (efg': forall y:Y, paths _ y (f (g y))). intros. set (e1:= efg y). apply pathsinv0. assumption. 
-apply (isweql2 Y Y (fun y:Y => (f (g y)))  (fun  y:Y => y)  efg' z (idisweq Y z)). 
-apply (isweql1 _ _ g f egf z). assumption. 
+Proof. intros.  unfold isweq.  intro y'.
+assert (iscontr (hfiber _ _ (fun y:Y => (f (g y))) y')). 
+assert (efg': forall y:Y, paths _ y (f (g y))). intro y. apply pathsinv0. exact (efg y).
+apply (isweql2 Y Y (fun y:Y => (f (g y)))  (fun  y:Y => y)  efg' y' (idisweq Y y')). 
+apply (isweql1 _ _ g f egf y'). assumption. 
 Defined.
  
 
